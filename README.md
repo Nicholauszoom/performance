8ec05493cd848e2234b490f1d55623dcc75469e7
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2022-11-23 15:19:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Fléx Performance
+
+About fléx performance
+
+Installation guide
+
+-   step 1 [npm install](https://cits.co.tz).
+-   step 2 [Powerful dependency injection container](https://laravel.com/docs/container).
+-   Run the migration and seeders
+-   Run the php artisan commance.
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoutingController;
+
+
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::group(['middleware' => 'auth', 'prefix' => '/'], function () {
+    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+    Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+    Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+});
