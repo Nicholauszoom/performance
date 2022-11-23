933953330e02154d57dc8888a7876cb75a14cf0b
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2022-11-23 17:26:16 +0300

    .

--- resources/views/pages/starter.blade.php
+++ resources/views/pages/starter.blade.php
@@ -7,14 +7,14 @@
 @section('content')
     <!-- Start Content-->
     <div class="container-fluid">
-        
+
         <!-- start page title -->
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box">
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Performance</a></li>
                             <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
                             <li class="breadcrumb-item active">Starter</li>
                         </ol>
@@ -22,9 +22,9 @@
                     <h4 class="page-title">Starter</h4>
                 </div>
             </div>
-        </div>     
-        <!-- end page title --> 
-        
+        </div>
+        <!-- end page title -->
+
     </div> <!-- container -->
 @endsection
 
