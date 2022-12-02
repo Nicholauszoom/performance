3a1bfa6913cf3cc9d46d67bccddd33aa4a79a75e
samwel herman <samwelherman85@gmail.com>
2022-12-02 11:29:31 +0300

    side bar changes

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -40,12 +40,12 @@
                 {{-- /Dashboard --}}
 
                 {{-- Projects --}}
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="ph-hard-drives"></i>
                         <span>Projects</span>
                     </a>
-                </li>
+                </li> -->
                 {{-- /projects --}}
 
 
@@ -60,20 +60,52 @@
                             <a
                                 href="{{ route('members.active') }}"
                                 class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
+                                                 >
+                               Employees Info Management
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
+                                               >
+                                               Transfers  Management
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
                             >
-                                Active Employees
+                            Attendance
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
+                            >
+                            Overtime management	
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
+                            >
+                            Grievances and Disciplinary Management
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}"
+                            >
+                            Performance & Productiviy Management
                             </a>
                         </li>
                     </ul>
                 </li>
-                {{-- /workforce management --}}
-
-
-                {{--
-                    Leave & Attendance
-                        -   Attendance
-                --}}
-
                 {{-- Payroll management --}}
                 <li class="nav-item nav-item-submenu">
                     <a href="#" class="nav-link">
@@ -148,6 +180,64 @@
                     </ul>
                 </li>
                 {{-- /Payroll management --}}
+                	
+
+
+
+                <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-users-three"></i>
+                        <span>Learning & Development</span>
+                    </a>
+
+                    <ul class="nav-group-sub collapse">
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}">
+                                Training Budgeting
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}">
+                                Training Application
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+                <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-users-three"></i>
+                        <span>Talent Management</span>
+                    </a>
+
+                    <ul class="nav-group-sub collapse">
+                       
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('members.active') }}"
+                                class="nav-link {{ request()->routeIs('members.active') ? 'active' : null  }}">
+                                Talent Management
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                {{-- /workforce management --}}
+                	
+
+
+                {{--
+                    Leave & Attendance
+                        -   Attendance
+                --}}
+
+                
 
                 {{--
                     Organisation
@@ -167,6 +257,22 @@
                         <span>Organisation</span>
                     </a>
                     <ul class="nav-group-sub collapse">
+                    <li class="nav-item">
+                            <a
+                                href="{{ route('departments.index') }}"
+                                class="nav-link {{ request()->routeIs('departments.index') ? 'active' : null  }}"
+                            >
+                            Company Registration
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('departments.index') }}"
+                                class="nav-link {{ request()->routeIs('departments.index') ? 'active' : null  }}"
+                            >
+                            Organization structure Registration
+                            </a>
+                        </li>
 
                         <li class="nav-item">
                             <a
@@ -182,20 +288,15 @@
                                 href="{{ route('designations.index') }}"
                                 class="nav-link {{ request()->routeIs('designations.index') ? 'active' : null  }}"
                             >
-                                Position
+                            Role Profile Registration(Position)
                             </a>
                         </li>
 
-                        <li class="nav-item">
-                            <a
-                                href="{{ route('users.index') }}"
-                                class="nav-link {{ request()->routeIs('users.index') ? 'active' : null  }}"
-                            >
-                               User Management
-                            </a>
-                        </li>
+                       
                     </ul>
                 </li>
+
+
                 {{-- /organisation --}}
 
                 {{--
@@ -226,14 +327,14 @@
                             </a>
                         </li>
 
-                        {{-- <li class="nav-item">
+                        <li class="nav-item">
                             <a
                                 href="{{ route('roles.index') }}"
                                 class="nav-link {{ request()->routeIs('roles.index') ? 'active' : null  }}"
                             >
                                 Roles
                             </a>
-                        </li> --}}
+                        </li> 
 
                         <li class="nav-item">
                             <a
@@ -241,6 +342,14 @@
                                 class="nav-link {{ request()->routeIs('permissions.index') ? 'active' : null  }}"
                             >
                                 Permissions
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="{{ route('users.index') }}"
+                                class="nav-link {{ request()->routeIs('users.index') ? 'active' : null  }}"
+                            >
+                               User Management
                             </a>
                         </li>
 
