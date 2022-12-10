d9e5793a85078fb07ff5715883150f73e621d04b
martinaAbdallah <martinaabdallah203@gmail.com>
2022-12-10 15:02:30 +0300

    account code

--- resources/views/app/company_info.blade.php
+++ resources/views/app/company_info.blade.php
@@ -1,80 +1,78 @@
-
-
-@extends('layouts.vertical', ['title' => 'Dashboard'])
+@extends('layouts.vertical', ['title' => 'Organisation'])
 
 @push('head-script')
-<script src="{{ asset('assets/js/vendor/tables/datatables/datatables.min.js') }}"></script>
-@endpush
+<script src="{{ asset('assets/js/components/tables/datatables/datatables.min.js') }}"></script>
 
-@push('head-scriptTwo')
-<script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
-@endpush
+    @endpush
 
-@section('content')('content')
+    @push('head-scriptTwo')
+    <script src="{{ asset('assets/js/pages/datatables_basic.js') }}"></script>
+    @endpush
 
-<?php
+@section('content')
+<div class="row">
+{{-- <div class="col-md-6"> --}}
+  <h3>Organization structure </h3>
 
-?>
+<div class="card">
+  <div class="card-header">
 
-         <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Organization Structure </h3>
-              </div>
-            </div>
+  <h4>Position Organization structure</h4>
+  </div>
+  <table class="table datatable-basic">
+      <thead>
+          <tr>
+              <th>S/N</th>
+              <th>Code</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th class="text-center">Actions</th>
+              {{-- <th>action</th> --}}
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><span class="badge bg-success bg-opacity-10 text-success"></span></td>
+              <td></td>
+              <td></td>
 
-            <div class="clearfix"></div>
+          </tr>
 
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2> Department Organization Structure</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <div class="chartpo" id="custom-colored_dept"></div>
-                  </div>
-                </div>
-              </div>
-              
-              
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2> Position Organization Structure</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <div class="chartpo" id="custom-colored"></div>
-                  </div>
-                </div>
-              </div>
-              
-              
 
-            </div>
-          </div>
-        </div>
-        <!-- /page content -->
-        
+      </tbody>
+  </table>
+</div>
+  <div class="card">
+    <div class="card-header">
+<h4>Department OrganizationStructure</h4>
+</div>
+  <table class="table datatable-basic">
+    <thead>
+        <tr>
+            <th>S/N</th>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th class="text-center">Actions</th>
+            {{-- <th>action</th> --}}
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><span class="badge bg-success bg-opacity-10 text-success"></span></td>
+            <td></td>
+            <td></td>
+        </tr>
 
-    @include("app/includes/organization_chart_footer");
-    @include("app/includes/organization-chart");
-    
-    
+
+    </tbody>
+</table>
+  </div>
+</div>
  @endsection
