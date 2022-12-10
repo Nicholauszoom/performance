e35b16a377aaa7e1f1575a7fd84570d61ad7934b
laisonmarko <laisonmarko@gmail.com>
2022-12-10 14:17:46 +0300

    update department

--- database/migrations/2022_12_10_105204_add_department_id_to_branch_table.php
+++ database/migrations/2022_12_10_105204_add_department_id_to_branch_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('branch', function (Blueprint $table) {
             //
-            $table->integer('department')->unsigned()->nullable()->after('id');
+            $table->integer('department_id')->unsigned()->nullable()->after('id');
 
         });
     }
