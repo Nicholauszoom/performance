5191d03d2ad2d17b078e2eed67ee87c914c1f62c
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2022-12-10 10:44:45 +0300

    left-sidebar

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -28,6 +28,7 @@
                     </a>
                 </li>
 
+
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/flex/project/') }}">
                         <i class="ph-hard-drives"></i>
@@ -97,6 +98,7 @@
                     </ul>
                 </li>
 
+
                 <li class="nav-item nav-item-submenu">
                     <a href="#" class="nav-link">
                         <i class="ph-calculator"></i>
@@ -104,20 +106,94 @@
                     </a>
 
                     <ul class="nav-group-sub collapse">
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/payroll"> Payroll </a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/payroll/payroll') }}"> Payroll </a></li>
                         @if (session('mng_paym'))
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/employee_payslip"> Payslip </a></li>
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/comission_bonus">Incentives</a></li>
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/partial_payment">Partial Payment</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/payroll/employee_payslip') }}"> Payslip </a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/payroll/comission_bonus') }}">Incentives</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/payroll/partial_payment') }}">Partial Payment</a></li>
                         @endif
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/approved_financial_payments">Pending Payments </a></li>
-                        <?php if (session('mng_stat_rpt')) { ?>
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/financial_reports">Statutory Reports </a></li>
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/organisation_reports">Organisation Reports </a></li>
-                        <?php } ?>
-                        <?php if (session('mng_paym')) { ?>
-                            <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/salary_calculator"> Salary Calculator </a></li>
-                        <?php } ?>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/approved_financial_payments') }}">Pending Payments </a></li>
+                        @if (session('mng_stat_rpt'))
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/financial_reports') }}">Statutory Reports </a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/organisation_reports') }}">Organisation Reports </a></li>
+                        @endif
+                        @if (session('mng_paym'))
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/payroll/salary_calculator') }}"> Salary Calculator </a></li>
+                        @endif
+                    </ul>
+                </li>
+
+
+                <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-buildings"></i>
+                        <span>Organisation</span>
+                    </a>
+                    <ul class="nav-group-sub collapse">
+
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/costCenter') }}">Cost Center </a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/department') }}">Departments </a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/branch') }}">Company Branches </a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/position') }}">Positions</a></li>
+                        @if (session('mng_emp'))
+                            <li class="nav-item"><a class="nav-link" href="/flex/contract">Employee Contracts</a></li>
+                        @endif
+                        {{-- <li  class="nav-item"><a class="nav-link"  href="{{ url('/flex/accountCoding') }}">Account Coding</a></li> --}}
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/organization_level') }}">Organisation Levels </a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/organization_structure') }}">Organisation Structure</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/flex/accounting_coding') }}">Accounting Coding</a></li>
+
+                    </ul>
+                </li>
+
+
+                <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-note"></i>
+                        <span>Reports</span>
+                    </a>
+                    <ul class="nav-group-sub collapse">
+                        {{-- <li class="nav-item">
+                            <a href="{{ route('cipay.financial_reports') }}" class="nav-link {{ request()->routeIs('cipay.financial_reports') ? 'active' : null  }}">
+                                Statutory Reports
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="{{ route('cipay.organisation_reports') }}" class="nav-link {{ request()->routeIs('cipay.organisation_reports') ? 'active' : null  }}">
+                                Organisation Reports
+                            </a>
+                        </li> --}}
+                    </ul>
+                </li>
+
+
+                <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-gear-six"></i>
+                        <span>Settings</span>
+                    </a>
+
+                    <ul class="nav-group-sub collapse">
+                        @if (session('mng_roles_grp'))
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/flex/role') }}">Roles and Groups</a></li>
+                        @endif
+                        <li class="nav-item"><a class="nav-link" href="/flex/allowance">Allowances</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/flex/allowance_overtime">Overtime</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/flex/statutory_deductions">Statutory Deductions</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/flex/non_statutory_deductions">Non-Statutory Deductions</a></li>
+
+                        @if (session('mng_bank_info'))
+                            <li class="nav-item"><a class="nav-link" href="/flex/bank">Banking Information</a></li>
+                        @endif
+
+                        @if (session('mng_audit'))
+                            <li class="nav-item"><a class="nav-link" href="/flex/audit_logs">Audit Trail</a></li>
+                        @endif
+
+                        <li class="nav-item"><a class="nav-link" href="/flex/performance/funder"></i> Funders </a></li>
+                        <li class="nav-item"><a class="nav-link" href="/flex/nationality">Nationality</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/flex/payroll/mailConfiguration"></i> Mail Configuration </a></li>
                     </ul>
                 </li>
 
@@ -126,115 +202,30 @@
 
 
 
-
-            {{-- Organisation --}}
-            <li class="nav-item nav-item-submenu">
-                <a href="#" class="nav-link">
-                    <i class="ph-buildings"></i>
-                    <span>Organisation</span>
-                </a>
-                <ul class="nav-group-sub collapse">
-
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/costCenter">Cost Center </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/department">Departments </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/branch">Company Branches </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/position">Positions</a></li>
-                    <?php if (session('mng_emp')) { ?>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/contract">Employee Contracts</a></li>
-                    <?php } ?>
-                    {{-- <li  class="nav-item"><a class="nav-link"  href="<?php echo url(""); ?>/flex/accountCoding">Account Coding</a></li> --}}
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/organization_level">Organisation Levels </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/organization_structure">Organisation Structure</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(""); ?>/flex/accounting_coding">Accounting Coding</a></li>
-
-                </ul>
-            </li>
-
-            <li class="nav-item nav-item-submenu">
-                <a href="#" class="nav-link">
-                    <i class="ph-buildings"></i>
-                    <span>Reports</span>
-                </a>
-                <ul class="nav-group-sub collapse">
-                    {{-- <li class="nav-item">
-                        <a href="{{ route('cipay.financial_reports') }}"
-                    class="nav-link {{ request()->routeIs('cipay.financial_reports') ? 'active' : null  }}">
-                    Statutory Reports
+                {{-- routes Pending Model --}}
+                {{-- <li class="nav-item nav-item-submenu">
+                    <a href="#" class="nav-link">
+                        <i class="ph-briefcase"></i>
+                        <span>Recruitment Management</span>
                     </a>
-            </li> --}}
-
-            {{-- <li class="nav-item">
-                        <a href="{{ route('cipay.organisation_reports') }}"
-            class="nav-link {{ request()->routeIs('cipay.organisation_reports') ? 'active' : null  }}">
-            Organisation Reports
-            </a>
-            </li> --}}
-
-            </ul>
-            </li>
-
-
-            {{-- /organisation --}}
-
-
-            {{-- settings --}}
-            <li class="nav-item nav-item-submenu">
-                <a href="#" class="nav-link">
-                    <i class="ph-gear-six"></i>
-                    <span>Settings</span>
-                </a>
-                <ul class="nav-group-sub collapse">
-
-                    <?php if (session('mng_roles_grp')) { ?>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/role">Roles and Groups</a></li>
-                    <?php } ?>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/allowance">Allowances</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/allowance_overtime">Overtime</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/statutory_deductions">Statutory Deductions</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/non_statutory_deductions">Non-Statutory Deductions</a></li>
-
-                    <?php if (session('mng_bank_info')) { ?>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/bank">Banking Information</a></li>
-
-                    <?php }
-                    if (session('mng_audit')) { ?>
-                        <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/audit_logs">Audit Trail</a></li>
-                    <?php } ?>
-
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/performance/funder"></i> Funders </a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/nationality">Nationality</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo url(''); ?>/flex/payroll/mailConfiguration"></i> Mail Configuration </a></li>
-
-
-                </ul>
-            </li>
-            <li class="nav-item nav-item-submenu">
-                <a href="#" class="nav-link">
-                    <i class="ph-briefcase"></i>
-                    <span>Recruitment Management</span>
-                </a>
-                <ul class="nav-group-sub collapse">
-                    <li class="nav-item">
-                        <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
-                            Job Post Management
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
-                            Job Application Management
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
-                            Interview Schedule Management
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            {{-- /settings --}}
-
-
-
+                    <ul class="nav-group-sub collapse">
+                        <li class="nav-item">
+                            <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
+                                Job Post Management
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
+                                Job Application Management
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('system.index') }}" class="nav-link {{ request()->routeIs('system.index') ? 'active' : null }}">
+                                Interview Schedule Management
+                            </a>
+                        </li>
+                    </ul>
+                </li> --}}
 
             </ul>
         </div>
