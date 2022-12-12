ef84f19ccb122cc93094318bdfa6c0b9b5a68bb5
laisonmarko <laisonmarko@gmail.com>
2022-12-12 13:06:20 +0300

    add logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,10 @@
                                 <div class="card-body">
                                     <div class="text-center mb-3">
                                         <div class="d-inline-flex align-items-center justify-content-center mb-4 mt-2">
-                                            <img src="{{ asset('login-assets/images/main-logo.png') }}" class="img-fluid rounded-circle" style="height: 10em" alt="logo">
+                                            <img src="{{ asset('assets/images/logo.png') }}" class="img-fluid rounded-circle" style="height: 10em" alt="logo">
+                                            <!-- <br>
+                                            <br>
+                                            <h style="color:#001949" >Performance<h/> -->
                                         </div>
 
                                     </div>
