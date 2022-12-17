a662c388046379ae2616a419c1f0da86ff21876b
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2022-12-17 11:58:15 +0300

    leave module

--- resources/views/app/attendees.blade.php
+++ resources/views/app/attendees.blade.php
@@ -1,166 +1,193 @@
-@extends('layouts.vertical', ['title' => 'Dashboard'])
+@extends('layouts.vertical', ['title' => 'Attandance'])
 
 @push('head-script')
-<script src="{{ asset('assets/js/vendor/tables/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/js/components/tables/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/js/components/ui/moment/moment.min.js') }}"></script>
+<script src="{{ asset('assets/js/components/pickers/daterangepicker.js') }}"></script>
+<script src="{{ asset('assets/js/components/pickers/datepicker.min.js') }}"></script>
 @endpush
 
 @push('head-scriptTwo')
-<script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
+<script src="{{ asset('assets/js/pages/datatables_basic.js') }}"></script>
 @endpush
 
 @section('content')
 
-<?php
-?>
+<div class="card">
+    <div class="card-header">
+        <h5 class="text-main">Attendance</h5>
+    </div>
 
-
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Attendance </h3>
-              </div>
-
-              <!-- <div class="title_right">
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                  </div>
-                </div>
-              </div> -->
-
-            </div>
-
-            <div class="clearfix"></div>
-
+    <div class="card-body">
+        <form id="printAttendance" method ="post" action="{{ url('/flex/reports/attendance') }}" target="_blank" class="form-horizontal form-label-left">
+            @csrf
             <div class="row">
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <form id="printAttendance" method ="post" action="<?php echo  url(''); ?>/flex/reports/attendance" target="_blank" class="form-horizontal form-label-left">
-                      <div class="form-group">
-                        <label class="control-label col-md-3  col-xs-6" >Employee Attendance ON</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                        <!--<div class="col-sm-6">-->
-                          <div class="input-group">
-                            <input type="text" name="start" class="form-control col-xs-12 has-feedback-left" id="single_cal3"  aria-describedby="inputSuccess2Status">
-                            <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
-                            <span class="input-group-btn">
-                              <button name="print"  class="btn btn-primary">PRINT</button>
-                            </span>
-                          </div>
-                          <!--</div>-->
-                        </div>
-                      </div>
-                      </form>
-                    <!-- <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul> -->
-
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content" id = "result">
-
-                    <table id="datatable" class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th><b>S/N</b></th>
-                          <th><b>Name</b></th>
-                          <th><b>Department</b></th>
-                          <th><b>Sign IN</b></th>
-                          <th><b>Sign OUT</b></th>
-                        </tr>
-                      </thead>
-
-
-                      <tbody>
-                        <?php
-
-                          foreach ($attendee as $row) { ?>
-                          <tr >
-                            <td width="1px"><?php echo $row->SNo; ?></td>
-                            <td><?php echo $row->name; ?></td>
-                            <td><?php echo "<b>Department: </b>".$row->DEPARTMENT."<br><b>Position: </b>".$row->POSITION; ?></td>
-                            <td><?php echo $row->time_in; ?></td>
-                            <td><?php  echo  $row->time_out; ?></td>
-                            </tr>
-                          <?php } //} ?>
-                      </tbody>
-                    </table>
-                  </div>
+                <div class="col-md-6">
+                    <label class="control-label mb-2" >Employee Attendance ON</label>
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="ph-calendar"></i></span>
+                        <input type="date" name="start" class="form-control daterange-single" id="single_cal3" value="{{ date('Y-m-d') }}">
+                        <button name="print"  class="btn btn-main">PRINT</button>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
+        </form>
+    </div>
+
+    <table  class="table table-striped table-bordered datatable-basic">
+        <thead>
+          <tr>
+            <th><b>S/N</b></th>
+            <th><b>Name</b></th>
+            <th><b>Department</b></th>
+            <th><b>Sign IN</b></th>
+            <th><b>Sign OUT</b></th>
+          </tr>
+        </thead>
+
+
+        <tbody>
+          <?php
+
+            foreach ($attendee as $row) { ?>
+            <tr >
+              <td width="1px"><?php echo $row->SNo; ?></td>
+              <td><?php echo $row->name; ?></td>
+              <td><?php echo "<b>Department: </b>".$row->DEPARTMENT."<br><b>Position: </b>".$row->POSITION; ?></td>
+              <td><?php echo $row->time_in; ?></td>
+              <td><?php  echo  $row->time_out; ?></td>
+              </tr>
+            <?php } //} ?>
+        </tbody>
+      </table>
+</div>
 
 
 
-       @include("app/includes/dropdown")
+@endsection
 
-      <script>
+@push('footer-script')
 
-      function testing() {
-        console.log('adfsd');
-        // notify('Sale list empty', 'top', 'right', 'success');
+<script type="text/javascript">
 
-      }
+    // var attandee_table = $('#datatable').DataTable({
+    //             bPaginate: true,
+    //             bInfo: true,
+    //             ordering: false,
+    //             columns: [
+    //                 {'data': 'SNo'},
+    //                 {'data': 'name'},
+    //                 {'data': 'DEPARTMENT'},
+    //                 {'data': 'time_in'},
+    //                 {'data': 'time_out'}
+    //             ]
+    //         });
 
-      function notify(message, from, align, type) {
-        $.growl({
-            message: message,
-            url: ''
-        }, {
-            element: 'body',
-            type: type,
-            allow_dismiss: true,
-            placement: {
-                from: from,
-                align: align
-            },
-            offset: {
-                x: 30,
-                y: 30
-            },
-            spacing: 10,
-            z_index: 999999,
-            delay: 2500,
-            timer: 1000,
-            url_target: '_blank',
-            mouse_over: false,
+    $(document).ready(function(){
+        $('#single_cal3').on('change',function(){
+            var formValue = $(this).val();
+            if(formValue){
+                $.ajax({
+                    type:'POST',
+                    url:'{{ url("/flex/attendance/attendeesFetcher/") }}',
+                    data:'due_date='+formValue,
+                    success:function(html){
+                      let jq_json_obj = $.parseJSON(html);
+                      let jq_obj = eval (jq_json_obj);
+                      attandee_table.clear();
+                      attandee_table.rows.add(jq_obj);
+                      attandee_table.draw();
 
-            icon_type: 'class',
-            template: '<div data-growl="container" class="alert" role="alert">' +
-                '<button type="button" class="close" data-growl="dismiss">' +
-                '<span aria-hidden="true">&times;</span>' +
-                '<span class="sr-only">Close</span>' +
-                '</button>' +
-                '<span data-growl="icon"></span>' +
-                '<span data-growl="title"></span>' +
-                '<span data-growl="message"></span>' +
-                '<a href="#!" data-growl="url"></a>' +
-                '</div>'
+                    }
+                });
+            }
         });
-      }
+    });
+    </script>
+
+
+<script>
+    $(document).ready(function() {
+
+      $('#department').on('change',function(){
+          var stateID = $(this).val();
+          if(stateID){
+              $.ajax({
+                  type:'POST',
+                  url:'{{ url("/flex/positionFetcher/") }}',
+                  data:'dept_id='+stateID,
+                  success:function(html){
+                      // $('#pos').html(html);
+                      let jq_json_obj = $.parseJSON(html);
+                      let jq_obj = eval (jq_json_obj);
+
+                      //populate position
+                      $("#pos option").remove();
+                      $('#pos').append($('<option>', {
+                          value: '',
+                          text: 'Select Position',
+                          selected: true,
+                          disabled: true
+                      }));
+                      $.each(jq_obj.position, function (detail, name) {
+                          $('#pos').append($('<option>', {value: name.id, text: name.name}));
+                      });
+                  }
+              });
+          }else{
+              // $('#pos').html('<option value="">Select state first</option>');
+          }
+      });
+    });
+  </script>
+
+
+<script>
+
+    function testing() {
+      console.log('adfsd');
+      // notify('Sale list empty', 'top', 'right', 'success');
+
+    }
+
+    function notify(message, from, align, type) {
+      $.growl({
+          message: message,
+          url: ''
+      }, {
+          element: 'body',
+          type: type,
+          allow_dismiss: true,
+          placement: {
+              from: from,
+              align: align
+          },
+          offset: {
+              x: 30,
+              y: 30
+          },
+          spacing: 10,
+          z_index: 999999,
+          delay: 2500,
+          timer: 1000,
+          url_target: '_blank',
+          mouse_over: false,
+
+          icon_type: 'class',
+          template: '<div data-growl="container" class="alert" role="alert">' +
+              '<button type="button" class="close" data-growl="dismiss">' +
+              '<span aria-hidden="true">&times;</span>' +
+              '<span class="sr-only">Close</span>' +
+              '</button>' +
+              '<span data-growl="icon"></span>' +
+              '<span data-growl="title"></span>' +
+              '<span data-growl="message"></span>' +
+              '<a href="#!" data-growl="url"></a>' +
+              '</div>'
+      });
+    }
 
 
 
-      </script>
-
- @endsection
+</script>
+@endpush
--- resources/views/app/leave_report.blade.php
+++ resources/views/app/leave_report.blade.php
@@ -11,201 +11,150 @@
 
 @section('content')
 
-<?php  
-  ?>
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="text-main">Leaves </h3>
+    </div>
+
+    <div class="card-body">
+        My Leave History Already Accepted
+    </div>
+
+    <table  class="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Nature</th>
+            <th>Duration(Days)</th>
+            <th>From</th>
+            <th>To</th>
+
+          </tr>
+        </thead>
 
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Leaves </h3>
-              </div>
-            </div>
+        <tbody>
+          <?php
+            foreach ($my_leave as $row) { ?>
+            <tr>
+              <td width="1px"><?php echo $row->SNo; ?></td>
+              <td><?php  echo $row->NAME; ?></td>
+              <td><?php echo "<b>DEPARTMENT:</b> ".$row->DEPARTMENT."<br><b>POSITION: </b>".$row->POSITION; ?></td>
+              <td><?php echo $row->TYPE; ?></td>
+              <td><?php echo $row->days; ?></td>
+              <td><?php  echo date('d-m-Y', strtotime($row->start));  ?></td>
+              <td <?php if($row->state==0){ ?> bgcolor="#F5B7B1" <?php } ?>>
+              <?php  echo date('d-m-Y', strtotime($row->end))."<br>"; ?>  </td>
+              </td>
 
-            <div class="clearfix"></div>
+              </tr>
 
-            <div class="row">
+            <?php }  ?>
+        </tbody>
+    </table>
+</div>
 
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>My Leave History Already Accepted</h2>
+@if (session('line')!= 0 || session('conf_leave')!= 0)
+    <div class="card">
+        <div class="card-body">
+            Other Employees' Leaves Already Accepted
 
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
+            <?php if (session('conf_leave')!= 0 ){ ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a><button type="button" id="modal" data-toggle="modal" data-target="#departmentModal" class="btn btn-success">LEAVE REPORT</button></a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="<?php echo  url(''); ?>/flex/attendance/customleavereport" ><button type="button" class="btn btn-success">View Individual Report</button></a> <?php } ?>
 
-                   @if(Session::has('note'))      {{ session('note') }}  @endif  ?>
-                  
-                    <table  class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>S/N</th>
-                          <th>Name</th>
-                          <th>Department</th> 
-                          <th>Nature</th>  
-                          <th>Duration(Days)</th>
-                          <th>From</th>
-                          <th>To</th>
-                          
-                        </tr>
-                      </thead>
-
-
-                      <tbody>
-                        <?php
-                          foreach ($my_leave as $row) { ?>
-                          <tr>
-                            <td width="1px"><?php echo $row->SNo; ?></td>
-                            <td><?php  echo $row->NAME; ?></td>
-                            <td><?php echo "<b>DEPARTMENT:</b> ".$row->DEPARTMENT."<br><b>POSITION: </b>".$row->POSITION; ?></td>
-                            <td><?php echo $row->TYPE; ?></td>
-                            <td><?php echo $row->days; ?></td>
-                            <td><?php  echo date('d-m-Y', strtotime($row->start));  ?></td>
-                            <td <?php if($row->state==0){ ?> bgcolor="#F5B7B1" <?php } ?>>
-                            <?php  echo date('d-m-Y', strtotime($row->end))."<br>"; ?>  </td>
-                            </td>
-
-                            </tr>
-
-                          <?php }  ?>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-
-            <?php if (session('line')!= 0 || session('conf_leave')!= 0 ) {   ?>
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Other Employees' Leaves Already Accepted 
-
-                    <?php if (session('conf_leave')!= 0 ){ ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a><button type="button" id="modal" data-toggle="modal" data-target="#departmentModal" class="btn btn-success">LEAVE REPORT</button></a>
-                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a href="<?php echo  url(''); ?>/flex/attendance/customleavereport" ><button type="button" class="btn btn-success">View Individual Report</button></a> <?php } ?>
-                    </h2>
-
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-
-                   @if(Session::has('note'))      {{ session('note') }}  @endif  ?>
-                  
-                    <table id="datatable" class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>S/N</th>
-                          <th>Name</th>
-                          <th>Department</th> 
-                          <th>Nature</th>  
-                          <th>Duration(Days)</th>
-                          <th>From</th>
-                          <th>To</th>
-                          <th>Status</th>
-                          
-                        </tr>
-                      </thead>
-
-
-                      <tbody>
-                        <?php
-                          foreach ($other_leave as $row) { ?>
-                          <tr>
-                            <td width="1px"><?php echo $row->SNo; ?></td>
-                            <td><?php  echo $row->NAME; ?></td>
-                            <td><?php echo "<b>DEPARTMENT:</b> ".$row->DEPARTMENT."<br><b>POSITION: </b>".$row->POSITION; ?></td>
-                            <td><?php echo $row->TYPE; ?></td>
-                            <td><?php echo $row->days; ?></td>
-                            <td><?php  echo date('d-m-Y', strtotime($row->start));  ?></td>
-                            <td><?php  echo date('d-m-Y', strtotime($row->end));  ?></td>
-                            <td>
-                              <?php if($row->end>=$today) echo '<span class="label label-success"><b>IN PROGRESS</b></span>'; else echo '<span class="label label-danger"><b>COMPLETED</b></span>';  ?>  
-                            </td>
-
-                            </tr>
-
-                          <?php } //} ?>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-              <?php } ?>
-
-            </div>
-          </div>
         </div>
 
+        <table id="datatable" class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Name</th>
+                <th>Department</th>
+                <th>Nature</th>
+                <th>Duration(Days)</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Status</th>
 
-          <!-- Modal -->
-                <div class="modal fade" id="departmentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                              <h4 class="modal-title" id="myModalLabel">View Custom Leave Report</h4>
-                          </div>
-                          <div class="modal-body">
-                          <!-- Modal Form -->
-                          <form id="demo-form2" enctype="multipart/form-data" target="_blank" method="post" action="<?php echo  url(''); ?>/flex/attendance/customleavereport"  data-parsley-validate class="form-horizontal form-label-left">
-                        
-
-                        <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">From
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <div class="has-feedback">
-                          <input type="text" name="from" class="form-control col-xs-12 has-feedback-left" id="single_cal1"  aria-describedby="inputSuccess2Status">
-                          <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
-                        </div>
-                        </div>
-                      </div> 
-
-                      <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">To
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <div class="has-feedback">
-                          <input type="text" name="to" class="form-control col-xs-12 has-feedback-left" id="single_cal2"  aria-describedby="inputSuccess2Status">
-                          <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
-                        </div>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name" for="stream" >Type</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <select name="target" class="form-control">
-                               <option value="1"> ALL LEAVES</option>
-                                <option value="2">COMPLETED</option>
-                                <option value="3">ON PROGRESS</option>
-                            </select>
-                        </div>
-                      </div> 
-
-                      
-                      <div class="modal-footer">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                          <input type="submit"  value="SHOW" name="view" class="btn btn-primary"/>
-                          <input type="submit"  value="PRINT" name="print" class="btn btn-success"/>
-                      </div>
-                      </form>
-                  </div>
-                  <!-- /.modal-content -->
-              </div>
-              <!-- /.modal-dialog -->
-            </div>
-          <!-- Modal Form -->          
-          </div>           
-          <!-- /.modal -->
-        <!-- /page content -->
+              </tr>
+            </thead>
 
 
+            <tbody>
+              <?php
+                foreach ($other_leave as $row) { ?>
+                <tr>
+                  <td width="1px"><?php echo $row->SNo; ?></td>
+                  <td><?php  echo $row->NAME; ?></td>
+                  <td><?php echo "<b>DEPARTMENT:</b> ".$row->DEPARTMENT."<br><b>POSITION: </b>".$row->POSITION; ?></td>
+                  <td><?php echo $row->TYPE; ?></td>
+                  <td><?php echo $row->days; ?></td>
+                  <td><?php  echo date('d-m-Y', strtotime($row->start));  ?></td>
+                  <td><?php  echo date('d-m-Y', strtotime($row->end));  ?></td>
+                  <td>
+                    <?php if($row->end>=$today) echo '<span class="label label-success"><b>IN PROGRESS</b></span>'; else echo '<span class="label label-danger"><b>COMPLETED</b></span>';  ?>
+                  </td>
 
-           
+                  </tr>
+
+                <?php } //} ?>
+            </tbody>
+          </table>
+    </div>
+@endif
 
 
- @endsection
+<div class="modal">
+    <form id="demo-form2" enctype="multipart/form-data" target="_blank" method="post" action="<?php echo  url(''); ?>/flex/attendance/customleavereport"  data-parsley-validate class="form-horizontal form-label-left">
+
+
+        <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">From
+        </label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+          <div class="has-feedback">
+          <input type="text" name="from" class="form-control col-xs-12 has-feedback-left" id="single_cal1"  aria-describedby="inputSuccess2Status">
+          <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
+        </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">To
+        </label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+          <div class="has-feedback">
+          <input type="text" name="to" class="form-control col-xs-12 has-feedback-left" id="single_cal2"  aria-describedby="inputSuccess2Status">
+          <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
+        </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name" for="stream" >Type</label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <select name="target" class="form-control">
+               <option value="1"> ALL LEAVES</option>
+                <option value="2">COMPLETED</option>
+                <option value="3">ON PROGRESS</option>
+            </select>
+        </div>
+      </div>
+
+
+      <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          <input type="submit"  value="SHOW" name="view" class="btn btn-primary"/>
+          <input type="submit"  value="PRINT" name="print" class="btn btn-success"/>
+      </div>
+      </form>
+</div>
+
+
+
+
+@endsection
