1e580cb0d2d420f9ae85b9d656b8475faaf34be0
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2023-01-02 16:43:06 +0300

    error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body{
+          margin-top: 150px;
+          background-color: #00204e;
+        }
+        .error-main{
+          background-color: #fff;
+          box-shadow: 0px 10px 10px -10px #00204e;
+        }
+        .error-main h1{
+          font-weight: bold;
+          color: #00204e;
+          font-size: 150px;
+          text-shadow: 2px 4px 5px #00204e;
+        }
+        .error-main h6{
+          color: #00204e;
+          font-size: 20px;
+        }
+        .error-main p{
+          color: #00204e;
+          font-size: 15px;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
+          <div class="row">
+            <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
+              <h1 class="m-0">404</h1>
+              <h6>Page not found</h6>
+              <p>If this problem persist please contact <a href="https://www.cits.co.tz/" target="_blank">Coorprate Information Technology</a>, to provide you with support.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</body>
+</html>
