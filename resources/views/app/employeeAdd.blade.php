50250cf29155c507b9f1eb81c1ffb957861640f5
Douglas Fortunatus <fortunatusdouglas@gmial.com>
2023-01-03 07:22:58 +0300

    job level

--- resources/views/app/employeeAdd.blade.php
+++ resources/views/app/employeeAdd.blade.php
@@ -320,8 +320,42 @@
 
                 <div class="col-md-4 col-lg-4">
                     <div class="mb-3">
-                        <label class="form-label">Level:</label>
-                        <input type="text" name="emp_level" value="{{ old('emp_level') }}" class="form-control emp_level @error('emp_level') is-invalid @enderror" maxlength="30" required>
+                        <label class="form-label">Job Level:</label>
+
+                        <select name="emp_level" id="" class="form-select select">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="25">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                        </select>
+
+                        {{-- <input type="text" name="emp_level" value="{{ old('emp_level') }}" class="form-control emp_level @error('emp_level') is-invalid @enderror" maxlength="30" required> --}}
                     </div>
                 </div>
             </div>
