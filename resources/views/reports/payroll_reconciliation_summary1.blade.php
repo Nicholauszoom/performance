d28939378c765f273671a9b329c97383d8326949
baltazar-christian <baltazarchristian49@gmail.com>
2023-01-25 23:09:13 +0300

    updated payroll reconcilliation

--- resources/views/reports/payroll_reconciliation_summary1.blade.php
+++ resources/views/reports/payroll_reconciliation_summary1.blade.php
@@ -8,6 +8,12 @@
     <title>Payroll Reconciliation-Summary</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        {{-- <link rel="stylesheet" href="{{ asset('assets/css/ltr/all.min.css') }}"> --}}
+
+
+
+
+
 
 </head>
 
@@ -19,14 +25,14 @@
             <div class="col-md-10 mx-auto">
 
                 <div class="row">
-                    <div class="col-md-7 col-5">
+                    <div class="col-md-7 col-7">
                         <div class="row">
-                            <div class="col-md-3">
+                            <div class="col-md-3 col-3">
                                 <img src="https://www.bancabc.co.tz/images/banc_abc_logo.png" alt="logo here"
                                     width="100%">
 
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-md-9 col-9">
                                 {{-- <br> --}}
                                 <p>AFRICAN BANKING CORPORATION <br>P.O. BOX 31<br>DAR ES SALAAM</p>
                                 <button onclick="window.print()">Print this page</button>
@@ -165,38 +171,38 @@
                         <div class="col-md-12 col-sm-12 col-12 p-2 mt-4" style="border:solid 1px gray ;border-bottom:none !important; ">
                             <p class="mt-2"><small><b>Prepared By</b></small></p>
 
+
                             <div class="row p-1">
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Name:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Position:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Signature:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Date_________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Name:______________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Position:__________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Signature:__________________________</div>
+                                <div class="col-md-6  col-6 col-lg-6 mb-3">Date_________________________</div>
                             </div>
                             <br>
                             <p><small><b>Checked and Approved By</b></small></p>
-
                             <div class="row p-1">
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Name:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Position:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Signature:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Date_________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Name:______________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Position:__________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Signature:__________________________</div>
+                                <div class="col-md-6  col-6 col-lg-6 mb-3">Date_________________________</div>
                             </div>
 
                             <p><small><b>Checked and Approved By</b></small></p>
 
                             <div class="row p-1">
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Name:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Position:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Signature:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Date_________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Name:______________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Position:__________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Signature:__________________________</div>
+                                <div class="col-md-6  col-6 col-lg-6 mb-3">Date_________________________</div>
                             </div>
 
                             <p><small><b>Approved By</b></small></p>
 
                             <div class="row p-1">
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Name:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Position:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Signature:__________________________</div>
-                                <div class="col-md-3 p-1 col-3 col-lg-3">Date_________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Name:______________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Position:__________________________</div>
+                                <div class="col-md-4  col-4 col-lg-4 mb-3">Signature:__________________________</div>
+                                <div class="col-md-6  col-6 col-lg-6 mb-3">Date_________________________</div>
                             </div>
                         </div>
                     </div>
@@ -211,6 +217,19 @@
 
 
 
+    <script src="{{ public_path('assets/js/jquery/jquery.min.js') }}"></script>
+    <script src="{{ public_path('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
