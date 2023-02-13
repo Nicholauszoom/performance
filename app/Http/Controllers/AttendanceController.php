e00ea8d0839e7da222735882ab456534dee3b106
laisonmarko <laisonmarko@gmail.com>
2023-02-13 13:07:28 +0300

    fix some error

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -184,11 +184,11 @@ elseif($nature == 6)
  $leave_balance =   $this->attendance_model->get_sick_leave_balance($empID,$nature,$year);
 
 }
-elseif($nature == 7)
-{
- $leave_balance =   $this->attendance_model-> ($empID,$nature,$year,$today);
+// elseif($nature == 7)
+// {
+//  $leave_balance =   $this->attendance_model->($empID,$nature,$year,$today);
 
-}
+// }
 
 
 
