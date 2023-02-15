e73f4d5dbfe0c959fb7f540555a8e5e69235c66b
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-02-16 02:14:00 +0300

    updated holiday

--- resources/views/setting/holidays.blade.php
+++ resources/views/setting/holidays.blade.php
@@ -16,18 +16,71 @@
 
 @section('content')
 
+{{-- start of add holiday form --}}
+
+{{-- start of all holidays table --}}
 <div class="card border-top  border-top-width-3 border-top-main rounded-0 p-2">
-    <div class="card-header border-0">
-        <div class="">
-            <h6 class="mb-0 text-muted">Holidays</h6>
 
+    <div id="save_termination" class="col-12 mx-auto">
+        <div class="card border-top  border-top-width-3 border-top-main border-bottom-main rounded-0">
+            <div class="card-header">
+                <h6 class="text-warning">Add Holiday</h6>
+            </div>
+            <div class="">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+                @endif
 
+                <form
+                    action="{{ route('flex.saveHoliday') }}"
+                    method="POST"
+                >
+                    @csrf
+
+                    <div class="card-body">
+                        <div class="row mb-1">
+                            <div class="col-6 col-lg-6">
+                                <div class="mb-1">
+                                    <label class="form-label">Holiday Name:</label>
+                                    <input type="text" name="name" id="" class="form-control" placeholder="Enter Holiday Name">
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-6">
+                                <div class="mb-1">
+                                    <label class="form-label">Holiday Date:</label>
+                                    <input type="date" name="date" id="" class="form-control" placeholder="Enter Date of Charge">
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-lg-12">
+                                <div class="mb-1">
+
+                                    <label for="recurring" class="form-label">Holiday Recurring:</label>
+                                    <input type="checkbox" name="recurring" id="recurring" class="check">
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-perfrom mb-1 float-end">Save Holiday</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    <hr>
     </div>
-    <div class="row mx-1">
+
+    <div class="row ">
         <div class="col-12">
-            <div class="card">
+            <div class="card border-top  border-top-width-3 border-top-main rounded-0">
+                <div class="card-header border-0">
+                    <div class="">
+                        <h6 class="mb-0 text-warning">Holidays</h6>
+            
+            
+                    </div>
+                <hr>
+                </div>
                 <table class="table datatable-basic">
                     <thead>
                         <th>SN</th>
@@ -48,9 +101,12 @@
                                     <a href="{{ route('flex.editholiday', base64_encode($item->id)) }}" class="btn btn-sm btn-main">
                                         <i class="ph-pen"></i>
                                     </a>
-                                    <a href="{{ route('flex.deleteholiday',$item->id) }}" class="btn btn-sm btn-danger">
-                                        <i class="ph-trash"></i>
-                                    </a>
+
+                                    <a href="javascript:void(0)" title="Cancel" class="icon-2 info-tooltip"
+                                    onclick="deleteHoliday(<?php echo $item->id; ?>)">
+                                    <button class="btn btn-danger btn-sm"><i class="ph-trash"></i> </button>
+                                     </a>
+
                                 </td>
                                 <td hidden>
 
@@ -66,54 +122,7 @@
         </div>
 
 
-        <div id="save_termination" class="col-7 mx-auto">
-            <div class="card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0">
-                <div class="card-header">
-                    <h6>Add Holiday</h6>
-                </div>
-                <div class="">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    <form
-                        action="{{ route('flex.saveHoliday') }}"
-                        method="POST"
-                    >
-                        @csrf
-
-                        <div class="card-body">
-                            <div class="row mb-1">
-                                <div class="col-6 col-lg-6">
-                                    <div class="mb-1">
-                                        <label class="form-label">Holiday Name:</label>
-                                        <input type="text" name="name" id="" class="form-control" placeholder="Enter Holiday Name">
-                                    </div>
-                                </div>
-                                <div class="col-6 col-lg-6">
-                                    <div class="mb-1">
-                                        <label class="form-label">Holiday Date:</label>
-                                        <input type="date" name="date" id="" class="form-control" placeholder="Enter Date of Charge">
-                                    </div>
-                                </div>
-
-                                <div class="col-12 col-lg-12">
-                                    <div class="mb-1">
-
-                                        <label for="recurring" class="form-label">Holiday Recurring:</label>
-                                        <input type="checkbox" name="recurring" id="recurring" class="check">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <button type="submit" class="btn btn-perfrom mb-1 float-end">Save Holiday</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+     
     </div>
 
 
@@ -127,30 +136,99 @@
 
 @push('footer-script')
 
-<script>
 
-$('#docNo').change(function(){
-    var id = $(this).val();
-    var url = '{{ route("getDetails", ":id") }}';
-    url = url.replace(':id', id);
 
-    $.ajax({
-        url: url,
-        type: 'get',
-        dataType: 'json',
-        success: function(response){
-            if(response != null){
-                $('#salary').val(response.salary+' '+response.currency);
-                $('#oldLevel').val(response.emp_level);
-                $('#oldPosition').val(response.position.name);
-            }
+    <script>
+
+
+
+
+        function deleteHoliday(id) {
+
+            Swal.fire({
+                title: 'Are You Sure You Want to Delete This Holiday ?',
+                // text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, Delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    var holidayid = id;
+
+                    $.ajax({
+                        url: "{{ url('flex/delete-holiday') }}/" + holidayid
+                    })
+                    .done(function(data) {
+                        $('#resultfeedOvertime').fadeOut('fast', function() {
+                            $('#resultfeedOvertime').fadeIn('fast').html(data);
+                        });
+
+                        $('#status' + id).fadeOut('fast', function() {
+                            $('#status' + id).fadeIn('fast').html(
+                                '<div class="col-md-12"><span class="label label-warning">CANCELLED</span></div>'
+                                );
+                        });
+
+                        // alert('Request Cancelled Successifully!! ...');
+
+                        Swal.fire(
+                            'Cancelled!',
+                            'Holiday Deleted Successifully!!.',
+                            'success'
+                        )
+
+                        setTimeout(function() {
+                            location.reload();
+                        }, 1000);
+                    })
+                    .fail(function() {
+                        Swal.fire(
+                            'Failed!',
+                            'Holiday Deletion Failed!! ....',
+                            'success'
+                        )
+
+                        alert('Holiday Deletion Failed!! ...');
+                    });
+                }
+            });
+
+            // if (confirm("Are You Sure You Want to Cancel This Overtime Request") == true) {
+
+            //     var overtimeid = id;
+
+            //     $.ajax({
+            //             url: "{{ url('flex/cancelOvertime') }}/" + overtimeid
+            //         })
+            //         .done(function(data) {
+            //             $('#resultfeedOvertime').fadeOut('fast', function() {
+            //                 $('#resultfeedOvertime').fadeIn('fast').html(data);
+            //             });
+
+            //             $('#status' + id).fadeOut('fast', function() {
+            //                 $('#status' + id).fadeIn('fast').html(
+            //                     '<div class="col-md-12"><span class="label label-warning">CANCELLED</span></div>'
+            //                     );
+            //             });
+
+            //             alert('Request Cancelled Successifully!! ...');
+
+            //             setTimeout(function() {
+            //                 location.reload();
+            //             }, 1000);
+            //         })
+            //         .fail(function() {
+            //             alert('Overtime Cancellation Failed!! ...');
+            //         });
+            // }
         }
-    });
-});
+    </script>
 
 
-</script>
 
+ 
 
 @endpush
 
