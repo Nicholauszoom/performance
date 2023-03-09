fd8503a19fd679eecc9fc9fe82ad15e6c4c8a632
samwel herman <samwelherman85@gmail.com>
2023-03-09 04:58:45 -0800

    Adding Roles Button

--- resources/views/manage/role/index.blade.php
+++ resources/views/manage/role/index.blade.php
@@ -14,9 +14,12 @@
                         <div class="card-header header-elements-sm-inline">
                             <h4 class="card-title">Roles</h4>
                             <div class="header-elements">
-                                <button type="button" class="btn btn-main" data-bs-toggle="modal"
+                                {{-- <button type="button" class="btn btn-main" data-bs-toggle="modal"
                                     data-bs-target="#addRoleModal">
                                     <i class="ph-plus me-2"></i>Add
+                                </button> --}}
+                                <button type="button" class="btn btn-main" data-bs-toggle="modal" data-bs-target="#add-finance-group">
+                                    <i class="ph-plus me-2"></i>New Group
                                 </button>
 
                             </div>
@@ -91,6 +94,43 @@
             {{-- @include('manage.role.edit') --}}
         </div>
     </section>
+    <div id="add-finance-group" class="modal fade" tabindex="-1">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">New Package(Group)</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <form action="{{ route("flex.financial_group") }}" method="POST" class="form-horizontal">
+                   @csrf
+                    <div class="modal-body">
+                        <div class="row mb-3">
+                            <label class="col-form-label col-sm-3">Group name</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="name" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-form-label col-sm-3">Group By</label>
+                            <div class="col-sm-9">
+                                <select class="form-control" name="grouped_by">
+                                    <option value="1">Employee</option>
+                                    <option value="2">Role</option>
+                                </select>
+
+                            </div>
+                        </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-perfrom">
+                         Save group
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <div class="modal fade" id="addRoleModal" tabindex="-1">
         <div class="modal-dialog" role="document">
             <div class="modal-content" id="modal-content">
