e8ab918929d66f4ae5d51f92132a5b808d00ede7
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-03-10 09:57:05 +0300

    updated

--- routes/api.php
+++ routes/api.php
@@ -52,7 +52,7 @@ Route::middleware('auth:sanctum')->group( function () {
       Route::post('/apply-leave',[LeaveController::class,'store']);
 
       // For Updating profile image
-      Route::post('/update-image',[GeneralController::class,'updateImg']);
+      Route::patch('/update-image',[GeneralController::class,'updateImg']);
 
 
    
