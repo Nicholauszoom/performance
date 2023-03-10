4ce382bde90d24dd8edae144995bc08b6cdaa42a
ibrahdrup <abrahamrupia@gmail.com>
2023-03-10 11:50:06 +0300

    undo handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,8 +48,5 @@ class Handler extends ExceptionHandler
             //
         });
     }
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        return response()->json(['error' => 'Unauthenticated.'], 401);
-    }
+   
 }
