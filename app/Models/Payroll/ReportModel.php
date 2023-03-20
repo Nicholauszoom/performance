66536aa51c31b819043c1bb10ba1ac77fee79989
martin kaboja <martinkabojja@gmail.com>
2023-03-20 09:17:42 +0300

    dte2

--- app/Models/Payroll/ReportModel.php
+++ app/Models/Payroll/ReportModel.php
@@ -1512,7 +1512,7 @@ and e.branch = b.code and e.line_manager = el.emp_id and c.id = e.contract_type 
         return $row + $row2;
     }
 
-    public function new_employee($date){
+    public function new_employee($date,$date2){
         $query = "SELECT count(id) from payroll_logs pl where pl.payroll_date = '".$date."' and pl.empID NOT IN (SELECT empID from payroll_logs where payroll_date = '".$date2."')";
    
 
