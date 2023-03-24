f37b0931bbdd212900d695746f3a62983f13b6b5
martin kaboja <martinkabojja@gmail.com>
2023-03-24 09:24:28 +0300

    termination

--- resources/views/workforce-management/terminal-balance.blade.php
+++ resources/views/workforce-management/terminal-balance.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Terminal Benefits</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+
         <style type="text/css">
             @media print {
                 #printbtn {
@@ -28,7 +28,7 @@
                     <div class="col-md-7 col-7">
                         <div class="row">
                             <div class="col-md-3 col-3">
-                                <img src="https://www.bancabc.co.tz/images/banc_abc_logo.png" alt="logo here"
+                                <img src="{{ asset('assets/images/logo-dif2.png') }}" alt="logo here"
                                     width="100%">
 
                             </div>
@@ -236,15 +236,12 @@
     </main>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-    </script>
+    <script src="{{ public_path('assets/js/jquery/jquery.min.js') }}"></script>
+    <script src="{{ public_path('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
 
 </body>
 
