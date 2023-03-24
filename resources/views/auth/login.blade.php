236d34b863e15c906ce634dfc96ab5b341f56552
martin kaboja <martinkabojja@gmail.com>
2023-03-24 13:52:01 +0300

    adding download

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -177,7 +177,7 @@
 
                                     </div>
                                     <div class="col-md-1  d-none d-md-block col-12 ">
-                                    <a href="{{ asset('img/slide2.png') }}">download</a>
+                                    <a href="{{ asset('img/slide2.png') }}" download>download</a>
                                     </div>
 
 
