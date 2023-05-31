9367e2149cd1359d0dcc0c59a42f2e41acd552b1
samwelherman85 <samwelherman85@gmail.com>
2023-05-31 14:32:52 +0300

    api testing

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -64,7 +64,7 @@ class AuthController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'User Created Successfully',
-                'token' => $user->createToken("API TOKEN")->plainTextToken
+                'token' => Hash::make($user->createToken("API TOKEN")->plainTextToken)
             ], 200);
 
         } catch (\Throwable $th) {
