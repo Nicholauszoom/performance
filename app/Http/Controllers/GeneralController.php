263cf1c8b37d22d4f99b50dd740d79b2d7154a1e
samwelherman85 <samwelherman85@gmail.com>
2023-05-31 12:27:30 +0300

    password error

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -6984,7 +6984,7 @@ class GeneralController extends Controller
                     'username' => $row->emp_id,
                     'password' => $pass,
                 );
-                Notification::route('mail', $row->email)->notify(new RegisteredUser($email_data));
+                Notification::route('mail', 'samwel.herman@cits.co.tz')->notify(new RegisteredUser($email_data));
             }
 
 
