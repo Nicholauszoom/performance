cba7e45bd763383cd0bcd0410161d7eb884821d7
samwelherman85 <samwelherman85@gmail.com>
2023-06-22 14:48:54 +0300

    payroll details

--- resources/views/reports/payroll_details.blade.php
+++ resources/views/reports/payroll_details.blade.php
@@ -31,6 +31,8 @@
                                     <p class="p-space">5th & 6th Floor, Uhuru Heights</p>
                                     <p class="p-space">Bibi Titi Mohammed Road</p>
                                     <p class="p-space">P.O. Box 31, Dar es salaam </p>
+                                    <p class="p-space">+255 22 22119422/2111990 </p>
+                                    <p class="p-space"> web:<a href="www.bancabc.co.tz">www.bancabc.co.tz</a></p>
 
                                 </div>
                             </td>
@@ -41,7 +43,7 @@
 
                             <td colspan="4" class="w-50" style="">
                                 <div class="box-text text-end">
-                                    <img src="{{ asset('assets/images/tdfl.png') }}" alt="logo here" width="180px" height="150px" class="image-fluid">
+                                    <img src="{{ asset('assets/images/logo-dif2.png') }}" alt="logo here" width="180px" height="150px" class="image-fluid">
                                 </div>
                             </td>
                         </tr>
