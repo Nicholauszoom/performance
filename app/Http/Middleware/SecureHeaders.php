7123d6cb7291a4c87f728fcbadcd2971e47b68e7
samwelherman85 <samwelherman85@gmail.com>
2023-07-04 13:34:24 +0300

    middleware

---
+++ app/Http/Middleware/SecureHeaders.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SecureHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //return $next($request);
+        $response = $next($request);
+
+        $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->header('X-Content-Type-Options', 'nosniff');
+        $response->header('X-Frame-Options', 'SAMEORIGIN');
+        $response->header('X-XSS-Protection', '1; mode=block');
+        $response->header('Content-Security-Policy', 'https://hc-hub.bancabc.co.tz');
+
+        return $response;
+    }
+}
