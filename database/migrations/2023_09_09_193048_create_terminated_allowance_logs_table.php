6afd656cdd98eda64c2b4ad3d40ddd57ba40e9d7
laisonmarko <laisonmarko@gmail.com>
2023-09-17 20:08:36 +0300

    update terminated alloawnce log

---
+++ database/migrations/2023_09_09_193048_create_terminated_allowance_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTerminateedAllowanceLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terminated_allowance_logs', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('empID', 10);
+            $table->integer('allowanceID')->default(6);
+            $table->string('description', 50)->default('Unclassified');
+            $table->string('policy', 50)->default('Fixed Amount');
+            $table->decimal('amount', 20, 2)->nullable();
+            $table->date('payment_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('allowance_logs');
+    }
+}
