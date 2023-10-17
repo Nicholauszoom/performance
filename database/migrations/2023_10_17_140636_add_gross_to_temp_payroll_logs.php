4066488dbe01ee398468a35fd320d6d7a88190fd
laisonmarko <laisonmarko@gmail.com>
2023-10-17 17:53:19 +0300

    update migration

--- database/migrations/2023_10_17_140636_add_gross_to_temp_payroll_logs.php
+++ database/migrations/2023_10_17_140636_add_gross_to_temp_payroll_logs.php
@@ -9,17 +9,10 @@ class AddGrossToTempPayrollLogs extends Migration
     public function up()
     {
 
-        $columnName = 'gross';
-        $tableName = 'temp_payroll_logs';
-
-        if (Schema::hasColumn($tableName, $columnName)) {
-            // The column exists in the table.
-
+        
         Schema::table('temp_payroll_logs', function (Blueprint $table) {
             $table->double('gross')->default(0.00);
         });
-
-    }
     }
 
     public function down()
