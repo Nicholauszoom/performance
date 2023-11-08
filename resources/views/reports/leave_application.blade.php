4f4f1d9245f35c9a1f21cd07359a2c105fd9db54
deogemini <geminideogratias8@gmail.com>
2023-11-08 20:06:58 +0300

    adding public_path in annual leave pending

--- resources/views/reports/leave_application.blade.php
+++ resources/views/reports/leave_application.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Annual Leave Report</title>
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/report.css') }}">
+    <link rel="stylesheet" href="{{ public_path('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ public_path('assets/css/report.css') }}">
 
 </head>
 
@@ -15,7 +15,7 @@
 
     <main class="body-font p-1">
         <div id="logo" style="margin-left: 7px; z-index: -10">
-            <img src="{{ asset('assets/images/x-left.png') }}" width="100px;" height="50px;">
+            <img src="{{ public_path('assets/images/x-left.png') }}" width="100px;" height="50px;">
         </div>
 
         <div style="margin-top:20px;">
@@ -44,7 +44,7 @@
 
                             <td colspan="4" class="w-50" style="">
                                 <div class="box-text text-end">
-                                    <img src="{{ asset('assets/images/logo-dif2.png') }}" alt="logo here" width="180px"
+                                    <img src="{{ public_path('assets/images/logo-dif2.png') }}" alt="logo here" width="180px"
                                         height="150px" class="image-fluid">
                                 </div>
                             </td>
@@ -131,7 +131,7 @@
 
 
         <div id="logo2" style="margin-left: 7px; z-index: -10">
-            <img src="{{ asset('assets/images/x-right.png') }}" width="100px;" height="50px;">
+            <img src="{{ public_path('assets/images/x-right.png') }}" width="100px;" height="50px;">
         </div>
 
     </main>
@@ -160,8 +160,8 @@
     <script src="{{ public_path('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
 
 
-    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
+    <script src="{{ public_path('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ public_path('assets/js/jquery/jquery.min.js') }}"></script>
 
 </body>
 
