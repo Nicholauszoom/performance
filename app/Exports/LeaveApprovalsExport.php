18c16a5368fcd051243522d3f4a2cd40c234d52c
hermesgido <hermessgido@gmail.com>
2023-11-10 14:45:03 +0300

    exel with headers

--- app/Exports/LeaveApprovalsExport.php
+++ app/Exports/LeaveApprovalsExport.php
@@ -4,9 +4,10 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class LeaveApprovalsExport implements FromCollection
-{
+class LeaveApprovalsExport implements FromCollection, WithHeadings{
+
     protected $approvals;
 
     public function __construct(Collection $approvals)
@@ -17,5 +18,19 @@ class LeaveApprovalsExport implements FromCollection
     public function collection()
     {
         return $this->approvals;
+    }
+
+    public function headings(): array
+    {
+        // Column headers based on your leave_approvals table structure
+        return [
+            'ID',
+            'Employee ID',
+            'Level 1',
+            'Level 2',
+            'Level 3',
+            'Escalation Time',
+            'Created At',
+        ];
     }
 }
