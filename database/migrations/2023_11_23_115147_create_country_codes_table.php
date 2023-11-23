02aadb14549fa235fcedafe529f7f59537794c1d
deogemini <geminideogratias8@gmail.com>
2023-11-24 00:07:46 +0300

    country codes added

---
+++ database/migrations/2023_11_23_115147_create_country_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('country_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_code');
+            $table->string('item_value');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('country_codes');
+    }
+};
