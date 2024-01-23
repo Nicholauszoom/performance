c75f33d01044bfe8d8513adcc95f883d1bbd0d30
ibrahdrup <abrahamrupia@gmail.com>
2024-01-23 11:28:50 +0300

    debugging bot api

--- app/Http/Controllers/BOTDataController.php
+++ app/Http/Controllers/BOTDataController.php
@@ -255,8 +255,9 @@ class BOTDataController extends Controller
                 $newres = json_encode($response);
 
                 // dd($newres);
-                    $employee =  Employee::all();
-                    $data['employee'] = $employee;
+                    // $employee =  Employee::all();
+                    // $data['employee'] = $employee;
+                    
                     return view('bot.index', compact('newres','data'));
             }
         }
