bf076d032dda28551382bb23f91e523adb0fe110
ibrahdrup <abrahamrupia@gmail.com>
2024-03-15 15:08:47 +0300

    position_category

---
+++ database/migrations/2024_03_15_093202_add_position_category_to_employee.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('position', function (Blueprint $table) {
+            $table->string('position_category')->nullable();
+        });
+        $table->foreign('position_category')->references('id')->on('position_category')->onDelete('cascade');
+    }
+
+    public function down()
+    {
+        Schema::table('position', function (Blueprint $table) {
+            $table->dropColumn('position_category');
+        });
+    }
+};
