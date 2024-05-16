181f718c547af93bc484b1c1c0efa77cf69b5b66
hermesgido <hermessgido@gmail.com>
2024-05-16 12:03:17 +0300

    password change image change

--- resources/views/auth/password-change.blade.php
+++ resources/views/auth/password-change.blade.php
@@ -1,77 +1,79 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @include('layouts.shared.title-meta', ['title' => "Change Password"])
 
-        <link rel="stylesheet" href="{{ asset('assets/fonts/inter/inter.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/icons/phosphor/styles.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/ltr/all.min.css') }}">
+<head>
+    @include('layouts.shared.title-meta', ['title' => 'Change Password'])
+
+    <link rel="stylesheet" href="{{ asset('assets/fonts/inter/inter.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/icons/phosphor/styles.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/ltr/all.min.css') }}">
 
 
-        <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 
-        <script src="{{ asset('assets/js/configurator.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/configurator.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
 
-        <script src="{{ asset('assets/js/app.js') }}"></script>
-    </head>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+</head>
 
-    @php
+@php
     $brandSetting = \App\Models\BrandSetting::first();
 @endphp
 
 
-    <style>
-        body {
-     /* background-image: url('{{ asset('img/bg.png') }}'); */
-     background-image: url('{{$brandSetting !=null && $brandSetting->body_background != null ? asset('storage/' . $brandSetting->body_background) : asset('img/bg.png') }}');
-     background-color: #cccccc;
-     
+<style>
+    body {
+        /* background-image: url('{{ asset('img/bg.png') }}'); */
+        background-image: url('{{ $brandSetting != null && $brandSetting->body_background != null ? asset('storage/' . $brandSetting->body_background) : asset('img/bg.png') }}');
+        background-color: #cccccc;
+
     }
-     </style>
-    <body class="bg-white">
+</style>
 
-        <div class="page-content">
-            <div class="content-wrapper">
-                <div class="content-inner">
-                    <div class="content d-flex justify-content-center align-items-center">
+<body class="bg-white">
 
-                        {{-- Login card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0--}}
-                        <form action="{{ route('password.update') }}" method="POST" class="login-form" autocomplete="off">
-                            @csrf
-                            @method('PUT')
+    <div class="page-content">
+        <div class="content-wrapper">
+            <div class="content-inner">
+                <div class="content d-flex justify-content-center align-items-center">
 
-                            <div class="card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0mb-0">
+                    {{-- Login card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0 --}}
+                    <form action="{{ route('password.update') }}" method="POST" class="login-form" autocomplete="off">
+                        @csrf
+                        @method('PUT')
 
-                                <div class="card-body">
-                                    <div class="text-center mb-3">
+                        <div
+                            class="card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0mb-0">
+
+                            <div class="card-body">
+                                <div class="text-center mb-3">
 
 
-                                        {{-- <div class="d-inline-flex align-items-center justify-content-center mb-4 mt-2">
+                                    {{-- <div class="d-inline-flex align-items-center justify-content-center mb-4 mt-2">
                                             <img src="{{ asset('assets/images/hc-hub-logo2.png') }}" class="img-fluid rounded-circle" style="height: 10em" alt="logo">
                                         </div> --}}
 
 
-                                        @if ($brandSetting->dashboard_logo)
-                                        <img src="{{ asset('storage/' . $brandSetting->dashboard_logo) }}" alt="flex logo" style="height: 10em" class="img-fluid">
-                
+                                    @if ($brandSetting->dashboard_logo)
+                                        <img src="{{ asset('storage/' . $brandSetting->dashboard_logo) }}"
+                                            alt="flex logo" style="height: 10em" class="img-fluid">
                                     @else
-                                    <img src="{{ asset('assets/images/hc-hub-logo2.png') }}" class="img-fluid rounded-circle" style="height: 10em" alt="logo">
-
-                
+                                        <img src="{{ asset('assets/images/hc-hub-logo2.png') }}"
+                                            class="img-fluid rounded-circle" style="height: 10em" alt="logo">
                                     @endif
 
 
+                                </div>
+
+                                @if (session()->has('status'))
+                                    <div class="alert alert-danger border-0 alert-dismissible fade show mb-3">
+                                        <span class="fw-semibold">Oh snap!</span> {{ session('status') }}.
+
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                                     </div>
-
-                                    @if (session()->has('status'))
-                                        <div class="alert alert-danger border-0 alert-dismissible fade show mb-3">
-                                            <span class="fw-semibold">Oh snap!</span> {{ session('status') }}.
-
-                                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                                        </div>
-                                    @else
-                                        @if ($errors->any())
+                                @else
+                                    @if ($errors->any())
                                         <div class="alert alert-danger border-0 alert-dismissible fade show mb-3">
                                             <ul>
                                                 @foreach ($errors->all() as $error)
@@ -81,90 +83,85 @@
 
                                             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                                         </div>
-                                        @endif
                                     @endif
+                                @endif
 
-                                    <div class="mb-3">
-                                        <label class="form-label text-main" for="current_password">Current Password</label>
+                                <div class="mb-3">
+                                    <label class="form-label text-main" for="current_password">Current Password</label>
 
-                                        <div class="form-control-feedback form-control-feedback-start">
-                                            <input
-                                                class="form-control @if($errors->has('current_password')) is-invalid @endif"
-                                                name="current_password"
-                                                type="password"
-                                                id="current_password"
-                                                required
-                                                value="{{ old('current_password')}}"
-                                                autocomplete="off"
-                                            >
+                                    <div class="form-control-feedback form-control-feedback-start">
+                                        <input class="form-control @if ($errors->has('current_password')) is-invalid @endif"
+                                            name="current_password" type="password" id="current_password" required
+                                            value="{{ old('current_password') }}" autocomplete="off">
 
-                                            <div class="form-control-feedback-icon">
-                                                <i class="ph-lock text-muted"></i>
-                                            </div>
+                                        <div class="form-control-feedback-icon">
+                                            <i class="ph-lock text-muted"></i>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="mb-3">
-                                        <label class="form-label text-main" for="password">New Password</label>
+                                <div class="mb-3">
+                                    <label class="form-label text-main" for="password">New Password</label>
 
-                                        <div class="form-control-feedback form-control-feedback-start">
-                                            <input
-                                                type="password"
-                                                id="password"
-                                                class="form-control @if($errors->has('password')) is-invalid @endif"
-                                                name="password"
-                                                value="{{ old('password') }}"
-                                                required
-                                                autocomplete="off"
-                                            >
+                                    <div class="form-control-feedback form-control-feedback-start">
+                                        <input type="password" id="password"
+                                            class="form-control @if ($errors->has('password')) is-invalid @endif"
+                                            name="password" value="{{ old('password') }}" required autocomplete="off">
 
-                                            <div class="form-control-feedback-icon">
-                                                <i class="ph-lock text-muted"></i>
-                                            </div>
+                                        <div class="form-control-feedback-icon">
+                                            <i class="ph-lock text-muted"></i>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="mb-3">
-                                        <label class="form-label text-main" for="password_confirmation">Confirm Password</label>
+                                <div class="mb-3">
+                                    <label class="form-label text-main" for="password_confirmation">Confirm
+                                        Password</label>
 
-                                        <div class="form-control-feedback form-control-feedback-start">
-                                            <input
-                                                type="password"
-                                                id="password_confirmation"
-                                                class="form-control @if($errors->has('password_confirmation')) is-invalid @endif"
-                                                name="password_confirmation"
-                                                value="{{ old('password_confirmation') }}"
-                                                required
-                                                autocomplete="off"
-                                            >
+                                    <div class="form-control-feedback form-control-feedback-start">
+                                        <input type="password" id="password_confirmation"
+                                            class="form-control @if ($errors->has('password_confirmation')) is-invalid @endif"
+                                            name="password_confirmation" value="{{ old('password_confirmation') }}"
+                                            required autocomplete="off">
 
-                                            <div class="form-control-feedback-icon">
-                                                <i class="ph-lock text-muted"></i>
-                                            </div>
+                                        <div class="form-control-feedback-icon">
+                                            <i class="ph-lock text-muted"></i>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="mb-3">
-                                        <button type="submit" class="btn btn-main w-100 border-0" style="background: #00204e">Log In</button>
-                                    </div>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-main w-100 border-0"
+                                        style="background: #00204e">Log In</button>
+                                </div>
 
-                                  </div>
                             </div>
-                        </form>
-                        {{-- /login card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0--}}
+                        </div>
+                    </form>
+                    {{-- /login card border-top border-bottom border-bottom-width-3 border-top-width-3 border-top-main border-bottom-main rounded-0 --}}
 
-                    </div>
                 </div>
             </div>
         </div>
-        <div class="mb-2">
-            <img src="{{ asset('img/logo.png') }}" class=" float-end"  style="height:2.4em;width:16%;float-left;opacity:100%" alt="logo">
+    </div>
 
-          </div>
-          <div class="">
+    <div class="mb-2">
+
+
+        @if ($brandSetting->company_logo)
+            <img src="{{ asset('storage/' . $brandSetting->company_logo) }}" class=" float-end"
+                style="height:2.4em;width:16%;float-left;opacity:100%" alt="logo">
+        @else
+            <img src="{{ asset('img/logo.png') }}" class=" float-end"
+                style="height:2.4em;width:16%;float-left;opacity:100%" alt="logo">
+        @endif
+    </div>
+
+
+    {{-- <div class="">
             <img src="{{ asset('img/top.png') }}" class=" float-end"  style="height:3.5em;width:100%;float-left;opacity:100%" alt="logo">
 
-         </div>
-    </body>
+         </div> --}}
+</body>
 
 </html>
