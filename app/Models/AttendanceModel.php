7ec559825b80a9f6622e1b2c6205df07e0d81c61
Deo Gemini <geminideogratias8@gmail.com>
2024-05-17 08:32:57 +0300

    🚀update accrued days formula

--- app/Models/AttendanceModel.php
+++ app/Models/AttendanceModel.php
@@ -504,6 +504,98 @@ class AttendanceModel extends Model
         return $maximum_days;
     }
 
+    public function calculate($accrual_rate, $hiredate,  $enddate)
+    {
+        $startDate = $hiredate;
+        $endDate = $enddate;
+        $accrualRate = $accrual_rate;
+
+        $dateValue =  explode('-', $startDate);
+
+        $startdatevalueNumber = $dateValue[2];
+        $startmonthvalueNumber = $dateValue[1];
+
+        $dateValue =  explode('-', $endDate);
+        $enddateValueNumber =  $dateValue[2];
+        $endmonthValueNumber =  $dateValue[1];
+
+        $d1 = new DateTime($startDate);
+        $d2 = new DateTime($endDate);
+
+                // Get the end of the start month
+        $endOfStartMonth = (clone $d1)->modify('last day of this month');
+
+        // Calculate days from start date to the end of the start month
+        $daysInStartMonth = $d1->diff($endOfStartMonth)->days + 1;
+
+        $calendar = $startDate;
+        $calendar2 = $endDate;
+        // Parse the date using Carbon
+        $carbonDate = Carbon::parse($calendar);
+        $carbonDate2 = Carbon::parse($calendar2);
+
+        // Get the number of days in the month
+        $start_date_month_numDays = $carbonDate->daysInMonth;
+        $end_date_month_numDays = $carbonDate2->daysInMonth;
+
+
+        if($startmonthvalueNumber == $endmonthValueNumber){
+            $days  = ($enddateValueNumber - $startdatevalueNumber)+1;
+            $Formula_part_A =  ($days *  $accrualRate) / $start_date_month_numDays;
+            $Formula_part_B = 0;
+            $Formula_part_C = 0;
+            return $Formula_part_A;
+           }else{
+            $days  = ($start_date_month_numDays - $startdatevalueNumber)+1;
+            $Formula_part_A =  ($days *  $accrualRate) / $start_date_month_numDays;
+           }
+
+        $startDateTime = new DateTime($startDate);
+
+        $endDateTime = new DateTime($endDate);
+
+        $startOfEndMonth = (clone $endDateTime)->modify('first day of this month');
+        $daysInEndMonth = ($startOfEndMonth->diff($endDateTime)->days)+1;
+
+        $Formula_part_C =    ($daysInEndMonth *  $accrualRate) / $end_date_month_numDays;
+
+
+         // Get the end of the start month
+         $endOfStartMonth = (clone $startDateTime)->modify('last day of this month');
+
+         // Calculate full months in between
+         $startOfNextMonth = (clone $endOfStartMonth)->modify('first day of next month');
+         $endOfPreviousMonth = (clone $endDateTime)->modify('first day of this month')->modify('last day of previous month');
+
+
+
+                // Calculate the month and year differences
+        $startMonth = (int)$startDateTime->format('m');
+        $startYear = (int)$startDateTime->format('Y');
+        $endMonth = (int)$endDateTime->format('m');
+        $endYear = (int)$endDateTime->format('Y');
+
+        // Determine if the dates are consecutive months
+        $isConsecutiveMonths = false;
+
+        if ($endYear === $startYear && $endMonth === $startMonth + 1) {
+            $isConsecutiveMonths = true;
+        } elseif ($endYear === $startYear + 1 && $startMonth === 12 && $endMonth === 1) {
+            $isConsecutiveMonths = true;
+        }
+
+        if ($isConsecutiveMonths) {
+            $Formula_part_B  = 0;
+        } else {
+
+            $fullMonths = ($startOfNextMonth->diff($endOfPreviousMonth)->m)+1;
+            $Formula_part_B =  $fullMonths * $accrualRate;
+
+        }
+        $totalAccruedDays =  $Formula_part_A + $Formula_part_B +  $Formula_part_C;
+        return $totalAccruedDays;
+    }
+
     function getAccruedBalance($empID, $hireDate, $today)
     {
 
@@ -515,7 +607,6 @@ class AttendanceModel extends Model
 
 
         $d1 = new DateTime($hireDate);
-
         $d2 = new DateTime($today);
 
         $diff = $d1->diff($d2);
@@ -523,6 +614,8 @@ class AttendanceModel extends Model
         $years = $diff->y;
         $months = $diff->m;
         $days = $diff->d;
+
+
 
          // Initialize accrued days
         $accrual_days = 0;
@@ -535,16 +628,19 @@ class AttendanceModel extends Model
                 if (date('Y-m-d') <= $employee->leave_effective_date) {
                     // If the current date is before or equal to the leave effective date
                     $old_accrual_rate = $employee->old_accrual_rate;
-                    $accrual_days = (($days * $old_accrual_rate) / 30) + $months * $old_accrual_rate + $years * 12 * $old_accrual_rate;
+                 //   $accrual_days = (($days * $old_accrual_rate``) / 30) + $months * $old_accrual_rate + $years * 12 * $old_accrual_rate;
+                    $accrual_days = $this->calculate($old_accrual_rate, $hireDate, $today);
                 } else {
                     $accrual_days = (($days * $employee->accrual_rate) / 30) + $employee->earlier_accrual_days;
                 }
             }else{
-                $accrual_days = (($days * $employee->accrual_rate) / 30) + $months * $employee->accrual_rate + $years * 12 * $employee->accrual_rate;
+                $accrual_rate =  $employee->accrual_rate;
+                $accrual_days = $this->calculate($accrual_rate, $hireDate, $today);
             }
         }
         else {
-            $accrual_days = (($days * $employee->accrual_rate) / 30) + $months * $employee->accrual_rate + $years * 12 * $employee->accrual_rate;
+           $accrual_rate =  $employee->accrual_rate;
+           $accrual_days = $this->calculate($accrual_rate, $hireDate, $today);
         }
         return $accrual_days;
     }
